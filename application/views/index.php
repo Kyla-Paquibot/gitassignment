9cff66bf82ca26d3db3a6f0a5e85babbb0648e0f
it3r4-paquibot <kylepaquibot@gmail.com>
2019-03-20 22:57:17 +0800

    My first Repository in Github

---
+++ application/views/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Git Assignment</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="http://localhost:81/catayas_git/assets/css/main.css">
+    <script src="http://localhost:81/catayas_git/assets/js/main.js"></script>
+</head>
+<body>
+   <h1>Welcome! This is your git Assigment</h1> 
+</body>
+</html>
